d5bbb66220df7480af0b152643d64db16ae4252a
DoroNess <doron@doroness.com>
2024-12-16 12:12:47 -0500

    Module 3 Implement API endpoints using Express-Mongo

--- server/database/app.js
+++ server/database/app.js
@@ -13,7 +13,6 @@ const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'))
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
-
 const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
@@ -58,17 +57,35 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
-});
+    try {
+      console.log('Received request for /fetchDealers');
+      const documents = await Dealerships.find();
+      console.log('Fetched dealers:', documents);
+      res.json(documents);
+    } catch (error) {
+      console.error('Error fetching dealerships:', error);
+      res.status(500).json({ error: 'Error fetching dealerships' });
+    }
+  });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships by state' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const document = await Dealerships.findOne({ id: req.params.id });
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealer by id' });
+  }
 });
 
 //Express route to insert review
